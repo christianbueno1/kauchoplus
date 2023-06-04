1b0411cc6963c6b1a3b7882b503386dbc6b63704
Christian Bueno <chmabuen@espol.edu.ec>
2023-06-03 23:06:26 -0500

    set theme Welding Workshop

---
+++ wp-content/themes/welding-workshop/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package Welding Workshop
+ * @subpackage welding_workshop
+ */
+
+?>
+<div class="site-info">
+  <div class="container">
+      <p><?php welding_workshop_credit(); ?> <?php echo esc_html(get_theme_mod('construction_hub_footer_text',__('By Themespride','welding-workshop'))); ?></p>
+  </div>
+</div>
